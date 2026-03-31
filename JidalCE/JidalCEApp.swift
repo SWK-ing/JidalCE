4fc6e4713e36da0cb180498270b71833773d4416
shinooya <yuk6mm@gmail.com>
2026-04-01 05:44:26 +0900

    Initial Commit

---
+++ JidalCE/JidalCEApp.swift
@@ -0,0 +1,17 @@
+//
+//  JidalCEApp.swift
+//  JidalCE
+//
+//  Created by SHINWOONG LEE on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct JidalCEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
